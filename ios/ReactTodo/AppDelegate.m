145069c62b0d5ca5e2b25672acc6d73648eb3f00
Marcos Castaneda <marcosjcastaneda@gmail.com>
2016-08-27 20:57:22 -0400

    Adds tunneled URL to JS location

--- ios/ReactTodo/AppDelegate.m
+++ ios/ReactTodo/AppDelegate.m
@@ -18,7 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [NSURL URLWithString:@"http://1d446966.ngrok.io/index.ios.bundle?platform=ios&dev=true"];
+//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ReactTodo"
